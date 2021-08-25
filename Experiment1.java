056b9d6e6f5fc1b105f1159f981999b95ef86ace
Ridhima <ridhima.aggarwal0326@gmail.com>
2021-08-26 04:38:07 +0530

    edited

---
+++ Experiment1.java
@@ -0,0 +1,8 @@
+package assignments;
+
+public class Experiment1 {
+    public static void main(String[] args) {
+        System.out.println("Hello ridz");
+    }
+    
+}
---
+++ Experiment2.java
@@ -0,0 +1,19 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class Experiment2 {
+    public static void main(String[] args) {
+       // Write a program to find the largest of 3 numbers.
+       Scanner in = new Scanner(System.in);
+        int num1, num2, num3;
+        int max = 0;
+        
+        // if( num1 > num2){
+        //     max = num1;
+        // } elif ()
+
+
+
+    }
+}
